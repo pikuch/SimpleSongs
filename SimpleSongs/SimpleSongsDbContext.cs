e99158c6ca2b919403545774ec2d91ac3bdda790
pikuch <55052790+pikuch@users.noreply.github.com>
2021-10-04 13:40:09 +0200

    Add database context class

---
+++ SimpleSongs/SimpleSongsDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleSongs.Model;
+using SimpleSongs.Resources;
+using System;
+
+namespace SimpleSongs
+{
+    /// <inheritdoc/>
+    class SimpleSongsDbContext : DbContext
+    {
+        DbSet<Song> Songs { get; set; }
+
+        /// <summary>
+        /// Configures the database relationships and seeds some data
+        /// </summary>
+        /// <param name="modelBuilder">ModelBuilder used to setup the model</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Song>().HasData(
+                new Song {
+                    Id = 1,
+                    Title = "Ciągnik",
+                    Author = "Blenders",
+                    AlbumName = "Fankomat",
+                    Length = new TimeSpan(0, 3, 40).TotalSeconds
+                },
+                new Song {
+                    Id = 2,
+                    Title = "Hydrogen",
+                    Author = "M.O.O.N.",
+                    AlbumName = "MOON EP",
+                    Length = new TimeSpan(0, 4, 31).TotalSeconds
+                },
+                new Song {
+                    Id = 3,
+                    Title = "...Baby One More Time",
+                    Author = "Britney Spears",
+                    AlbumName = "...Baby One More Time",
+                    Length = new TimeSpan(0, 3, 30).TotalSeconds
+                }
+                );
+        }
+
+        /// <summary>
+        /// Configures the database connection
+        /// </summary>
+        /// <param name="optionsBuilder">DbContextOptionsBuilder used for database context configuration</param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(SimpleStringResource.ConnectionString);
+        }
+    }
+}
