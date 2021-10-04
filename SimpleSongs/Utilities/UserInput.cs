78ef4351fd5afdee1604ec60c7e660061df7250c
pikuch <55052790+pikuch@users.noreply.github.com>
2021-10-04 15:18:09 +0200

    Add user input utility class

---
+++ SimpleSongs/Utilities/UserInput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SimpleSongs.Utilities
+{
+    public static class UserInput
+    {
+
+        public static string GetString(string prompt, int minLength, int maxLength)
+        {
+            string input = String.Empty;
+            bool isValid = false;
+
+            Console.Clear();
+            while (!isValid)
+            {
+                Console.WriteLine(prompt);
+                input = Console.ReadLine();
+                if (input.Length < minLength)
+                {
+                    Console.WriteLine($"String too short (<{minLength})");
+                }
+                else if (input.Length > maxLength)
+                {
+                    Console.WriteLine($"String too long (>{maxLength})");
+                }
+                else
+                {
+                    isValid = true;
+                }
+            }
+            return input;
+        }
+    }
+}
