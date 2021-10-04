5331688ccd3e679e9e512196a066c1b766f14a7c
pikuch <55052790+pikuch@users.noreply.github.com>
2021-10-04 15:07:12 +0200

    Add SongDao class

---
+++ SimpleSongs/DAL/SongDao.cs
@@ -0,0 +1,23 @@
+﻿using SimpleSongs.Model;
+
+namespace SimpleSongs.DAL
+{
+    /// <summary>
+    /// Provides data access methods for songs
+    /// </summary>
+    public class SongDao : ISongDao
+    {
+        /// <summary>
+        /// Add a new song to the database
+        /// </summary>
+        /// <param name="song">song to add</param>
+        public void AddSong(Song song)
+        {
+            using (var context = new SimpleSongsDbContext)
+            {
+                context.Songs.Add(song);
+                context.SaveChanges();
+            }
+        }
+    }
+}
